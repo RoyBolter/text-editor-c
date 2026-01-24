4b58aede1cc223ca83693c780867502c2427eb30
RoyBolter <ro465439@ucf.edu>
2026-01-24 04:23:53 -0500

    stopped ctrl-c and ctrl-z from doing what they do

--- text-editor.c
+++ text-editor.c
@@ -6,6 +6,8 @@
 
 struct termios orig_termios;
 
+// Raw mode allows the terminal to read the bytes of the characters input as they are typed 
+// rather than the default canonical mode which waits for the enter key to be pressed
 void disableRawMode() {
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
@@ -15,6 +17,7 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
   raw.c_lflag &= ~(ECHO | ICANON);
 
@@ -23,7 +26,8 @@ void enableRawMode() {
 
 int main() {
   enableRawMode();
-
+  
+  // Read and print the input byte in decimal form with the character afterwards
   char c;
   while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
     if (iscntrl(c)) {
