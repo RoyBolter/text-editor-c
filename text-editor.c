577e9aed9d760a548e70b12d5950cd78d34742a5
RoyBolter <ro465439@ucf.edu>
2026-01-24 04:40:40 -0500

    finished configurations to fully enter raw mode

--- text-editor.c
+++ text-editor.c
@@ -6,6 +6,7 @@
 
 struct termios orig_termios;
 
+
 // Raw mode allows the terminal to read the bytes of the characters input as they are typed 
 // rather than the default canonical mode which waits for the enter key to be pressed
 void disableRawMode() {
@@ -17,24 +18,33 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
-  raw.c_lflag &= ~(ECHO | ICANON);
+  // Disable flags from certain commands like ctrl-c or ctrl-z
+  raw.c_iflag &= ~(IXON | ICRNL | BRKINT | INPCK | ISTRIP);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= ~(CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
   enableRawMode();
-  
-  // Read and print the input byte in decimal form with the character afterwards
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+
+  while (1) {
+    // Read and print the input byte in decimal form with the character afterwards
+    char c = '\0';
+    read(STDIN_FILENO, &c, 1);
+
     if (iscntrl(c)) {
-      printf("%d\n", c);
+      printf("%d\r\n", c);
     } else {
-      printf("%d ('%c')\n",c,c);
+      printf("%d ('%c')\r\n",c,c);
     }
+    if (c == 'q') break;
   }
   return 0;
 }
